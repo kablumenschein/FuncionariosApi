e06beaf7971c74ddc7b5cd973131edb3fa08f314
kablumenschein <ka.blumenschein@hotmail.com>
2026-08-20 16:01:47 -0300

    Exercício 03 - Entidade Funcionario

--- 04-Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace _04_Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ 04-Domain/Entities/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace _04_Domain.Entities
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Cargo { get; set; }
+
+        [Required]
+        public string Departamento { get; set; }
+
+        [Range(0.01, double.MaxValue)]
+        public decimal Salario { get; set; }
+
+        public bool Ativo { get; set; } = true;
+    }
+}
