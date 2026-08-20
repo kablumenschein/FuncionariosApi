e6aad02f3c16b2236ad05e7e0379923fbda2999f
kablumenschein <ka.blumenschein@hotmail.com>
2026-08-20 17:30:53 -0300

    Exercício 09 - Connection string e Program.cs

--- 01-Presentation/Program.cs
+++ 01-Presentation/Program.cs
@@ -1,3 +1,10 @@
+using _02_Application.Interfaces;
+using _02_Application.Services;
+using _03_Infrastructure.Data;
+using _03_Infrastructure.Repositories;
+using _04_Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,9 +14,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IFuncionarioRepository, FuncionarioRepository>();
+builder.Services.AddScoped<IFuncionarioService, FuncionarioService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +29,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
+
