9c771f756d5f95f73e252789f3941496fe36a19a
kablumenschein <ka.blumenschein@hotmail.com>
2026-08-20 17:00:10 -0300

    Exercício 07 - FuncionariosController

---
+++ 01-Presentation/Controllers/FuncionariosController.cs
@@ -0,0 +1,25 @@
+﻿using _02_Application.DTOs;
+using _02_Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _01_Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/funcionarios")]
+    public class FuncionariosController : ControllerBase
+    {
+        private readonly IFuncionarioService _service;
+
+        public FuncionariosController(IFuncionarioService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(FuncionarioInputDto dto)
+        {
+            var criado = await _service.CreateAsync(dto);
+            return StatusCode(201, criado);
+        }
+    }
+}
