ece4921496c77cb61fa2ba70f8c5f3a5812ecf8b
kablumenschein <ka.blumenschein@hotmail.com>
2026-08-20 17:14:58 -0300

    Exercício 08 - AppDbContext e FuncionarioRepository

---
+++ 03-Infrastructure/Repositories/FuncionarioRepository.cs
@@ -0,0 +1,40 @@
+﻿using _03_Infrastructure.Data;
+using _04_Domain.Entities;
+using _04_Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace _03_Infrastructure.Repositories
+{
+    public class FuncionarioRepository : IFuncionarioRepository
+    {
+        private readonly AppDbContext _context;
+
+        public FuncionarioRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Funcionario>> GetAllAsync()
+            => await _context.Funcionarios.ToListAsync();
+
+        public async Task<Funcionario> GetByIdAsync(int id)
+            => await _context.Funcionarios.FindAsync(id);
+
+        public async Task AddAsync(Funcionario funcionario)
+            => await _context.Funcionarios.AddAsync(funcionario);
+
+        public void Update(Funcionario funcionario)
+            => _context.Funcionarios.Update(funcionario);
+
+        public void Delete(Funcionario funcionario)
+            => _context.Funcionarios.Remove(funcionario);
+
+        public async Task SaveChangesAsync()
+            => await _context.SaveChangesAsync();
+    }
+}
